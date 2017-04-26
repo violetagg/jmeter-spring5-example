5a9252a6cc45019d6d7ed69e2878933eca9bc2f8
Violeta Georgieva <vgeorgieva@pivotal.io>
2017-04-26 11:17:29 +0300

    Add ramp down time configuration

--- jmeter-plugin/src/main/java/com/example/jmeter/plugin/reactive/sampler/ReactiveResultCollector.java
+++ jmeter-plugin/src/main/java/com/example/jmeter/plugin/reactive/sampler/ReactiveResultCollector.java
@@ -11,6 +11,10 @@ import org.reactivestreams.Subscription;
 public class ReactiveResultCollector extends ResultCollector {
 
     private static final long serialVersionUID = 1L;
+
+    private static final String RAMP_DOWN_TIME = "ramp.down.time";
+    private static final int RAMP_DOWN_TIME_DEFAULT = 0; // milliseconds
+
     private static final AtomicIntegerFieldUpdater<ReactiveResultCollector> COUNTER_UPDATER = AtomicIntegerFieldUpdater
             .newUpdater(ReactiveResultCollector.class, "counter");
     private volatile int counter = 0;
@@ -41,15 +45,21 @@ public class ReactiveResultCollector extends ResultCollector {
 
             @Override
             public void onComplete() {
-                ReactiveResultCollector.super.sampleOccurred(event);
-                COUNTER_UPDATER.getAndDecrement(ReactiveResultCollector.this);
+                try {
+                    ReactiveResultCollector.super.sampleOccurred(event);
+                } finally {
+                    COUNTER_UPDATER.getAndDecrement(ReactiveResultCollector.this);
+                }
             }
 
             @Override
             public void onError(Throwable throwable) {
-                result.errorResult(throwable);
-                ReactiveResultCollector.super.sampleOccurred(event);
-                COUNTER_UPDATER.getAndDecrement(ReactiveResultCollector.this);
+                try {
+                    result.errorResult(throwable);
+                    ReactiveResultCollector.super.sampleOccurred(event);
+                } finally {
+                    COUNTER_UPDATER.getAndDecrement(ReactiveResultCollector.this);
+                }
             }
         });
     }
@@ -57,16 +67,16 @@ public class ReactiveResultCollector extends ResultCollector {
     @Override
     public void testEnded(String host) {
         System.out.println("samples left: " + counter);
-        /*
-        while (counter > 0) {
-            System.out.println("samples left: " + counter);
+        int rampDownTime = getPropertyAsInt(RAMP_DOWN_TIME, RAMP_DOWN_TIME_DEFAULT);
+        while (counter > 0 && rampDownTime > 0) {
+            rampDownTime -= 100;
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        */
+
         super.testEnded(host);
     }
 
